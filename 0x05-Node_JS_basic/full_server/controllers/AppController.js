e2aea69627b48fd3cdb67cf87ec433fdfb230b8c
THATO <138755182+Stormshadow96@users.noreply.github.com>
2024-09-17 22:50:06 +0200

    Update AppController.js

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,10 +1,10 @@
 /**
- *  * Contains the miscellaneous route handlers.
- *   */
+ * Contains the miscellaneous route handlers.
+ */
 class AppController {
-	  static getHomepage(request, response) {
-		      response.status(200).send('Hello Holberton School!');
-		    }
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
 }
 
 export default AppController;
