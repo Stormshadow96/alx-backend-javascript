e5e319d5a6f31cef437ea4078f9777c84521fe03
THATO <138755182+Stormshadow96@users.noreply.github.com>
2024-09-17 21:51:53 +0200

    Add files via upload

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,9 @@
 /**
- *  * Task 0 Displays message on STDOUT.
- *   * @param {String} msg Message to be displayed,
- *    * should be string value,
- *     * Example: Hello NodeJS
- *      */
+ * Task 0 Displays message on STDOUT.
+ * @param {String} msg Message to be displayed,
+ * should be string value,
+ * Example: Hello NodeJS
+ */
 const displayMessage = (msg) => {
 	  console.log(msg);
 };
